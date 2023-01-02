3eab567591513c5b9c7508239a77c39e3488980b
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-01-02 11:56:22 +0530

    Create Evaluate_Reverse_Polish_Notation.java

---
+++ Microsoft/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,30 @@
+Question: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < tokens.length; i++){
+            if(tokens[i].equals("+")){
+                int num2 = st.pop();
+                int num1 = st.pop();
+                st.push(num1 + num2);
+            }else if(tokens[i].equals("-")){
+                int num2 = st.pop();
+                int num1 = st.pop();
+                st.push(num1 - num2);
+            }else if(tokens[i].equals("*")){
+                int num2 = st.pop();
+                int num1 = st.pop();
+                st.push(num1 * num2);
+            }else if(tokens[i].equals("/")){
+                int num2 = st.pop();
+                int num1 = st.pop();
+                st.push(num1 / num2);
+            }else{
+                st.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        
+        return st.pop();
+    }
+}
