bac214f0760b9121945030141beb957ce5c3cf45
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-01-02 13:56:10 +0530

    Create Bulls_and_Cows.java

---
+++ Microsoft/Bulls_and_Cows.java
@@ -0,0 +1,41 @@
+// Question: leetcode.com/problems/bulls-and-cows/
+
+class Solution {
+    public String getHint(String secret, String guess) {
+        int[] nums = new int[10];
+        
+        for(char ch: secret.toCharArray()){
+            nums[ch - '0']++;
+        }
+        
+        int bulls = 0, cows = 0;
+        
+        for(int i = 0; i < secret.length(); i++){
+            int num = guess.charAt(i) - '0';
+            if(nums[num] > 0 && guess.charAt(i) == secret.charAt(i)){
+                bulls++;
+                nums[num]--;
+            }
+        }
+        
+        for(int i = 0; i < secret.length(); i++){
+            int num = guess.charAt(i) - '0';
+            if(guess.charAt(i) == secret.charAt(i)){
+                continue;
+            }
+            if(nums[num] > 0){
+                cows++;
+                nums[num]--;
+            }
+        }
+        
+        String ans = bulls + "A" + cows + "B";
+        
+        return ans;
+    }
+}
+
+/*
+TC  -  O(N) // N = Secret.length
+SC - O(10) = O(1)
+*/
